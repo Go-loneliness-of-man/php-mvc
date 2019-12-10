a86452cdaa8ccaa64f0cdc6a5f409f34c8c3caad
wangchao <1786053661@qq.com>
2019-12-10 18:50:15 +0800

    完善 publicService

--- core/publicService.php
+++ core/publicService.php
@@ -24,6 +24,34 @@ abstract class publicService {
       'total' => $total === 0 ? null : $total
     ]);
   }
+
+  // 常用代码片段，增，参数依次是模型、消息、要插入的数据（关联数组）
+  public function oftenC($model, $message = 'success', $body) {
+    $res = $model->create($body);                                                          // 插入记录
+    return $this->res(200, $message, $res);                                                // 返回固定格式
+  }
+
+  // 常用代码片段，删，参数依次是模型、消息、删除条件
+  public function oftenD($model, $message = 'success', $where) {
+    $res = $model->delete($where);                                                         // 删除记录
+    return $this->res(200, $message, $res);                                                // 返回固定格式
+  }
+
+  // 常用代码片段，改，参数依次是模型、消息、修改对象、修改条件
+  public function oftenU($model, $message = 'success', $obj, $where) {
+    foreach($obj as $k => $v)                                                              // 遍历修改所有属性
+      $res = $model->update($k, $v, $where);
+    return $this->res(200, $message, $res);                                                // 返回固定格式
+  }
+
+  // 常用代码片段，查，参数依次是模型、消息、id 列表
+  public function oftenR($model, $message = 'success', $list = []) {
+    if(count($list) > 0)
+      $res = $model->get($list);
+    else
+      $res = $model->dao->query("SELECT * FROM $model->dbname.$model->table");
+    return $this->res(200, $message, $res);                                                // 返回固定格式
+  }
 }
 
 
